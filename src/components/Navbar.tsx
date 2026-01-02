fe25c662813b521d33ebd2b7ab52b233f67d38cd
mvdevelop <marcosvmdilly@gmail.com>
2026-01-02 16:06:15 -0300

    feat: adding mobilemenu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,19 @@
 import Link from "next/link";
 import Image from "next/image";
 import icon from "@/app/icon.png";
+import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between">
+    <div className="h-24 flex items-center justify-between">
       {/* Left */}
       <div className="flex items-center gap-2">
         <Link href="/" className="flex items-center gap-2">
           <Image
             src={icon}
             alt="Macaw logo"
-            width={24}
-            height={24}
+            width={35}
+            height={35}
             priority
           />
           <span>Macaw</span>
@@ -24,7 +25,9 @@ const Navbar = () => {
       <div className="hidden"></div>
 
       {/* Right */}
-      <div className=""></div>
+      <div className="">
+        <MobileMenu />
+      </div>
     </div>
   );
 };
