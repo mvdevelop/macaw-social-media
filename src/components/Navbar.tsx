1e9528c3fdbd4e26bbf620c5ba14a4389f5b489d
mvdevelop <marcosvmdilly@gmail.com>
2026-01-02 15:23:13 -0300

    feat: working on navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,32 @@
 
-
+import Link from "next/link";
+import Image from "next/image";
+import icon from "@/app/icon.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
-}
+  return (
+    <div className="flex items-center justify-between">
+      {/* Left */}
+      <div className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-2">
+          <Image
+            src={icon}
+            alt="Macaw logo"
+            width={24}
+            height={24}
+            priority
+          />
+          <span>Macaw</span>
+        </Link>
+      </div>
+
+      {/* Center */}
+      <div className="hidden"></div>
+
+      {/* Right */}
+      <div className=""></div>
+    </div>
+  );
+};
 
 export default Navbar;
