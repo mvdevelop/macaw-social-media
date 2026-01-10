81fa2e8faec9f8d786d357689724ab491c22c315
mvdevelop <marcosvmdilly@gmail.com>
2026-01-09 21:23:09 -0300

    feat: working on addpost

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,11 +1,13 @@
 
-
+import Image from "next/image";
 
 const AddPost = () => {
   return (
-    <div className="">
+    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
       {/* Avatar */}
-      <div className=""></div>
+      <Image src="https://images.pexels.com/photos/35565461/pexels-photo-35565461.jpeg?_gl=1*1lkg175*_ga*NDg3ODk1ODQxLjE3Njc5MTQ5NDY.*_ga_8JE65Q40S6*czE3NjgwMDIyNDAkbzIkZzEkdDE3NjgwMDIyODckajEzJGwwJGgw" alt="" width={48} height={48} className="w-12 h-12 object-cover rounded-full">
+
+      </Image>
 
       {/* Post */}
       <div className="">
