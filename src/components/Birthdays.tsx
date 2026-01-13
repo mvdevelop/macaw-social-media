7e6e0059dfbb239336c1d277f7303b6938d27c88
mvdevelop <marcosvmdilly@gmail.com>
2026-01-13 15:37:48 -0300

    feat: working on birthdays

--- src/components/Birthdays.tsx
+++ src/components/Birthdays.tsx
@@ -1,10 +1,26 @@
 
 // src/components/Birthdays.tsx
 
+import Image from "next/image";
+
 const Birthdays = () => {
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm">
-      Birthdays
+      {/* Top */}
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">Birthdays</span>
+      </div>
+
+      {/* User 01 */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image src="https://images.pexels.com/photos/35496265/pexels-photo-35496265.jpeg" alt="Horizonte de Nova York à noite" width={40} height={40} className="w-10 h-10 rounded-full object-cover" />
+          <span className="font-semibold">Angelina Jolie</span>
+        </div>
+        <div className="flex gap-3 justify-end">
+          <button className="bg-blue-500 text-white text-xs px-2 ">Celebrate</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -13,12 +13,38 @@ const FriendRequests = () => {
         <Link href='/' className="text-blue-500 text-xs">See all</Link>
       </div>
 
-      {/* User */}
-      <div className="flex items-center justify-center">
-        <div className="">
-          <Image src="" alt="" width={10} height={10} className="" />
+      {/* User 01 */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image src="https://images.pexels.com/photos/35496265/pexels-photo-35496265.jpeg" alt="Horizonte de Nova York à noite" width={40} height={40} className="w-10 h-10 rounded-full object-cover" />
+          <span className="font-semibold">Angelina Jolie</span>
         </div>
-        <div className=""></div>
+        <div className="flex gap-3 justify-end">
+          <Image src="/accept.png" alt="Horizonte de Nova York à noite" width={20} height={20} className="cursor-pointer" />
+          <Image src="/reject.png" alt="Horizonte de Nova York à noite" width={20} height={20} className="cursor-pointer" />
+        </div>
+      </div>
+      {/* User 02 */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image src="https://images.pexels.com/photos/35525012/pexels-photo-35525012.jpeg?_gl=1*tl40ld*_ga*NDg3ODk1ODQxLjE3Njc5MTQ5NDY.*_ga_8JE65Q40S6*czE3NjgzMjQyMTckbzckZzAkdDE3NjgzMjQyMTckajYwJGwwJGgw" alt="" width={40} height={40} className="w-10 h-10 rounded-full object-cover" />
+          <span className="font-semibold">Julia Roberts</span>
+        </div>
+        <div className="flex gap-3 justify-end">
+          <Image src="/accept.png" alt="Horizonte de Nova York à noite" width={20} height={20} className="cursor-pointer" />
+          <Image src="/reject.png" alt="Horizonte de Nova York à noite" width={20} height={20} className="cursor-pointer" />
+        </div>
+      </div>
+      {/* User 03 */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image src="https://images.pexels.com/photos/35590309/pexels-photo-35590309.jpeg?_gl=1*1p4dky4*_ga*NDg3ODk1ODQxLjE3Njc5MTQ5NDY.*_ga_8JE65Q40S6*czE3NjgzMjQyMTckbzckZzAkdDE3NjgzMjQyMTckajYwJGwwJGgw" alt="" width={40} height={40} className="w-10 h-10 rounded-full object-cover" />
+          <span className="font-semibold">Michele Obama</span>
+        </div>
+        <div className="flex gap-3 justify-end">
+          <Image src="/accept.png" alt="Horizonte de Nova York à noite" width={20} height={20} className="cursor-pointer" />
+          <Image src="/reject.png" alt="Horizonte de Nova York à noite" width={20} height={20} className="cursor-pointer" />
+        </div>
       </div>
     </div>
   );
