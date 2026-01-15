8764c119ab91ae9e1b0736365cbdb0446ec8f321
mvdevelop <marcosvmdilly@gmail.com>
2026-01-15 14:48:54 -0300

    feat: working on usermediacard

--- src/components/UserInfoCard.tsx
+++ src/components/UserInfoCard.tsx
@@ -19,23 +19,35 @@ const UserInfoCard = ({ userId } : { userId: string }) => {
         <div className="flex flex-col gap-4 text-gray-500">
           <div className="flex items-center gap-2">
             <span className="text-xl text-black">Lloyd Fleming</span>
-            <span className="text-sm">@Jessica</span>
+            <span className="text-sm">@Lloyd</span>
           </div>
           <p>
             Lloyd Fleming is a software developer who enjoys working with React and TypeScript. Lloyd is passionate about creating efficient and scalable web applications.
           </p>
           <div className="flex items-center gap-2">
             <Image src='/map.png' alt="" width={16} height={16} />
-            <span>Living in <b>Denver</b></span>
+            <span>Living in <b>Denver.</b></span>
           </div>
           <div className="flex items-center gap-2">
-            <Image src='/map.png' alt="" width={16} height={16} />
-            <span>Went to <b>Edgar High School</b></span>
+            <Image src='/school.png' alt="" width={16} height={16} />
+            <span>Went to <b>Edgar High School.</b></span>
           </div>
           <div className="flex items-center gap-2">
-            <Image src='/map.png' alt="" width={16} height={16} />
+            <Image src='/work.png' alt="" width={16} height={16} />
             <span>Works at <b>Apple Inc.</b></span>
           </div>
+          <div className="flex items-center justify-between">
+            <div className="flex gap-1 items-center">
+              <Image src='/link.png' alt="" width={16} height={16} />
+              <Link href="https://macaw.com" className="text-blue-500 font-medium">macaw.com</Link>
+            </div>
+            <div className="flex gap-1 items-center">
+              <Image src='/date.png' alt="" width={16} height={16} />
+              <span>Joined January 2023</span>
+            </div>
+          </div>
+          <button className="bg-blue-500 text-white text-sm rounded-md p-2 cursor-pointer">Follow</button>
+          <span className="text-red-400 self-end text-xs cursor-pointer">Block User</span>
         </div>
       </div>
     </>
