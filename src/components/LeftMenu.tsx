fdec45f9315fc831560ad80bb41d05c8f87a72fd
mvdevelop <marcosvmdilly@gmail.com>
2026-01-15 17:00:06 -0300

    feat: working on profilecard

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -4,6 +4,7 @@
 import Link from "next/link";
 import ProfileCard from "./ProfileCard";
 import Image from "next/image";
+import Ad from "./Ad";
 
 const LeftMenu = ({type}:{type: "home" | "profile"}) => {
   return (
@@ -17,59 +18,60 @@ const LeftMenu = ({type}:{type: "home" | "profile"}) => {
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
 
         <Link href='/' className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
-          <Image src='/posts.png' alt='' width={20} height={20} />
-          <span>My Posts</span>
+          <Image src='/activity.png' alt='' width={20} height={20} />
+          <span>Activity</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
 
         <Link href='/' className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
-          <Image src='/posts.png' alt='' width={20} height={20} />
-          <span>My Posts</span>
+          <Image src='/market.png' alt='' width={20} height={20} />
+          <span>Marketplace</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
 
         <Link href='/' className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
-          <Image src='/posts.png' alt='' width={20} height={20} />
-          <span>My Posts</span>
+          <Image src='/events.png' alt='' width={20} height={20} />
+          <span>Events</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
 
         <Link href='/' className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
-          <Image src='/posts.png' alt='' width={20} height={20} />
-          <span>My Posts</span>
+          <Image src='/albums.png' alt='' width={20} height={20} />
+          <span>Albums</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
 
         <Link href='/' className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
-          <Image src='/posts.png' alt='' width={20} height={20} />
-          <span>My Posts</span>
+          <Image src='/videos.png' alt='' width={20} height={20} />
+          <span>Videos</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
 
         <Link href='/' className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
-          <Image src='/posts.png' alt='' width={20} height={20} />
-          <span>My Posts</span>
+          <Image src='/news.png' alt='' width={20} height={20} />
+          <span>News</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
 
         <Link href='/' className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
-          <Image src='/posts.png' alt='' width={20} height={20} />
-          <span>My Posts</span>
+          <Image src='/courses.png' alt='' width={20} height={20} />
+          <span>Courses</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
 
         <Link href='/' className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
-          <Image src='/posts.png' alt='' width={20} height={20} />
-          <span>My Posts</span>
+          <Image src='/lists.png' alt='' width={20} height={20} />
+          <span>Lists</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
 
         <Link href='/' className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100">
-          <Image src='/posts.png' alt='' width={20} height={20} />
-          <span>My Posts</span>
+          <Image src='/settings.png' alt='' width={20} height={20} />
+          <span>Settings</span>
         </Link>
-        <hr className="border-t-1 border-gray-50 w-36 self-center" />
+        {/* <hr className="border-t-1 border-gray-50 w-36 self-center" /> */}
       </div>
+      <Ad size="sm" />
     </div>
   );
 };
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,13 +1,39 @@
 
 // ProfileCard.tsx
 
+import Image from "next/image";
+
 
 
 const ProfileCard = () => {
   return (
     <>
-      <div className="">
-        Hello
+      <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
+        {/* <div className="h-20 relative">
+          <Image src='https://images.pexels.com/photos/17584747/pexels-photo-17584747.jpeg' alt='' fill className="rounded-md object-cover" />
+          <Image src='https://images.pexels.com/photos/35554037/pexels-photo-35554037.jpeg' alt='' fill className="rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10" />
+        </div> */}
+        <div className="h-20 relative">
+          {/* imagem de fundo */}
+          <div className="absolute inset-0">
+            <Image
+              src="https://images.pexels.com/photos/17584747/pexels-photo-17584747.jpeg"
+              alt=""
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+
+          {/* avatar */}
+          <div className="absolute left-0 right-0 m-auto -bottom-6 w-12 h-12 z-10 rounded-full ring-1 ring-white overflow-hidden">
+            <Image
+              src="https://images.pexels.com/photos/35554037/pexels-photo-35554037.jpeg"
+              alt=""
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
