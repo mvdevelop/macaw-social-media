86a12952beb7622409e6113040130d7a9167e44e
mvdevelop <marcosvmdilly@gmail.com>
2026-01-18 15:13:53 -0300

    feat: working on profilepage

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -17,12 +17,19 @@ const ProfilePage = () => {
           <div className="flex flex-col items-center justify-center">
             <div className="w-full h-64 relative">
               <Image
-                src="https://images.pexels.com/photos/35684097/pexels-photo-35684097.jpeg"
+                src="https://images.pexels.com/photos/2504709/pexels-photo-2504709.jpeg"
                 alt="Descrição da imagem"
                 fill
                 className="rounded-md object-cover"
               />
+              <Image
+                src="https://images.pexels.com/photos/35350413/pexels-photo-35350413.jpeg"
+                alt="Descrição da imagem" width={128} height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
+              />
             </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">Drak Lians</h1>
+            <div className="">Software Engineer</div>
           </div>
           <Feed />
         </div>
