74895323f3f18f1fff092ff1cab0eaf21dba044a
mvdevelop <marcosvmdilly@gmail.com>
2026-01-18 11:28:07 -0300

    feat: working on profilecard

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -9,7 +9,7 @@ const ProfilePage = () => {
   return (
     <div className="flex gap-6 pt-6">
       <div className="hidden xl:block w-[20%]">
-        <LeftMenu type={"home"} />
+        <LeftMenu type={"profile"} />
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -3,24 +3,16 @@
 
 import Image from "next/image";
 
-
-
 const ProfileCard = () => {
   return (
     <>
       <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
-        {/* <div className="h-20 relative">
-          <Image src='https://images.pexels.com/photos/17584747/pexels-photo-17584747.jpeg' alt='' fill className="rounded-md object-cover" />
-          <Image src='https://images.pexels.com/photos/35554037/pexels-photo-35554037.jpeg' alt='' fill className="rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10" />
-        </div> */}
         <div className="h-20 relative">
           {/* imagem de fundo */}
           <div className="absolute inset-0">
             <Image
               src="https://images.pexels.com/photos/17584747/pexels-photo-17584747.jpeg"
-              alt=""
-              fill
-              className="object-cover rounded-md"
+              alt="" fill className="rounded-md object-cover"
             />
           </div>
 
@@ -28,10 +20,22 @@ const ProfileCard = () => {
           <div className="absolute left-0 right-0 m-auto -bottom-6 w-12 h-12 z-10 rounded-full ring-1 ring-white overflow-hidden">
             <Image
               src="https://images.pexels.com/photos/35554037/pexels-photo-35554037.jpeg"
-              alt=""
-              fill
-              className="object-cover"
+              alt="" width={48} height={48}
+              className="rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto"
             />
+          </div>
+
+        </div>
+        <div className="h-20 flex flex-col gap-2 items-center">
+          <span className="font-semibold">Drak Lians</span>
+          <div className="flex items-center gap-4">
+            <div className="flex">
+              <Image
+              src="https://images.pexels.com/photos/35554037/pexels-photo-35554037.jpeg"
+              alt="" width={12} height={12}
+              className="rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto"
+              />
+            </div>
           </div>
         </div>
       </div>
